e1ccc731ae100b12b5913d63e3267740865d82d2
Devilzac <shane199274@gmail.com>
2019-11-14 14:51:49 +0100

    added getters and setters

--- index.php
+++ index.php
@@ -3,10 +3,8 @@ use Calc\NumToRomanic;
 
 require "./vendor/autoload.php";
 
-print_r(substr("adef", 0,1));
 
 
 $roman = new NumToRomanic();
-var_dump(substr(55, 2,1));
-var_dump($roman->calculate(51));
-
+var_dump($roman->calculate(10));
+?>
--- src/NumToRomanic.php
+++ src/NumToRomanic.php
@@ -2,8 +2,11 @@
 namespace Calc;
 
 class NumToRomanic{
-    public $numbers = [1=>"I", 5=>"V", 10=>"X", 50=>"L", 100=>"C", 500=>"D", 1000=>"M"];
-    public $romanic = ["I"=>1, "V"=>5, "X"=>10, "L"=>50, "C"=>100, "D"=>500, "M"=>1000];
+    public $romanic = ['(_-C)'=>100000000,'(_-X_-C)'=>90000000,'(_-L)'=>50000000,'(_-X_-L)'=>40000000,'(_-X)'=>10000000,'(_-M_-X)'=>9000000,'(_-V)'=>5000000,'(_-M_-V)'=>4000000,'(_M)'=>1000000,'(_C_M)'=>900000,'(_D)'=>500000,'(_C_D)'=>400000,'(_C)'=>100000,'(_X_C)'=>90000,'(_L)'=>50000,'(_X_L)'=>40000,'(_X)'=>10000,'(M_X)'=>9000, '(_V)'=>5000, '(M_V)'=>4000, 'M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    public $nRomano="";
+    public $valorIntroducido=0;
+    public $romanoActual="";
+    public $numeroActual=0;
 
     function getRomanicNumbers()
     {
@@ -14,25 +17,77 @@ class NumToRomanic{
     {
         return array_keys($this->numbers);
     }
-    function calculate($value){
-        $total;
-        for ($i=0; $i < strlen($value); $i++) {            
-            if(substr($value, 0,1) == 5){
-                if(substr(55, 2,1))
-                return $total= $this->numbers[$value];
-            }
-        }
-/*
-        if (is_numeric($value)) {
-            return $this->numbers[$value];
-        }
 
-        if (is_string($value)) {
-            return $this->romanic[$value];
-        }
-        */
-
-        
+    function setValorIntroducido($value)
+    {
+        $this->valorIntroducido=$value;
     }
+
+    function getValorIntroducido()
+    {
+        return $this->valorIntroducido;
+    }
+
+    function getNumeroRomano()
+    {
+        return $this->nRomano;
+    }
+
+    function setRomanoActual($valor)
+    {
+        $this->romanoActual=$valor;
+    }
+
+    function getRomanoActual()
+    {
+        return $this->romanoActual;
+    }
+
+    function setNumeroActual($valor)
+    {
+        $this->numeroActual=$valor;
+    }
+
+    function getNumeroActual()
+    {
+        return $this->numeroActual;
+    }
+
+
+
+
+
+
+    function calculate($value){ 
+        $this->setValorIntroducido($value);
+        while($this->getValorIntroducido()>0){
+          $this->transform();
+        }
+
+        return $this->getNumeroRomano();
+
+    }
+     function transform()
+     {
+
+         foreach ($this->romanic as $roman => $number) {
+            $this->setNumeroActual($number);
+            $this->setRomanoActual($roman);
+            $this->checkIfBigger();
+         }
+     }
+
+     function checkIfBigger()
+     {
+        if ($this->getValorIntroducido() >= $this->getNumeroActual()) {
+            $this.setRoman();
+        }
+     }
+     function setRoman(){
+        $numero=$this->getValorIntroducido();
+        $nuevoNumero = $numero - $this->getNumeroActual();
+        $this->setValorIntroducido($nuevoNumero);
+        $this->nRomano.=$this->getRomanoActual();
+     }
 
 }
